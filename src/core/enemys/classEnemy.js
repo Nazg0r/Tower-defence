4bf02279b4436aadcc87516506682f22612f4e48
Nazg0r <gorchinshiy83@gmail.com>
2023-06-06 23:50:51 +0300

    added sprite drawing of enemies

--- src/core/enemys/classEnemy.js
+++ src/core/enemys/classEnemy.js
@@ -1,20 +1,50 @@
 export default class Enemy {
-  constructor(x = -200, y = 0, width, height, speed = 1, ctx, waypoints) {
+  constructor(x = -200, y = 0, speed = 1, ctx, waypoints, enemySet) {
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = height;
     this.speed = speed;
     this.waypoints = waypoints;
     this.weypointIndex = 0;
     this.offset = 0;
     this.ctx = ctx;
-
+    this.image = new Image();
+    this.image.src = enemySet.imageSrc;
+    this.currentFrame = 0;
+    this.enemySet = {
+      imageSrc: enemySet.imageSrc,
+      frameStartY: enemySet.frameStartY,
+      frames: enemySet.frames,
+      spriteWidth: enemySet.spriteWidth,
+      spriteHeight: enemySet.spriteHeight,
+      hold: 5,
+      holdCounter: 0,
+    };
   }
 
   draw() {
-    this.ctx.fillStyle = 'red';
-    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    const frameStartX = this.enemySet.spriteWidth * this.currentFrame;
+    const correctX = this.x - this.enemySet.spriteWidth / 2;
+    const correctY = this.y - this.enemySet.spriteHeight / 2;
+
+    this.ctx.drawImage(
+      this.image,
+      frameStartX,
+      this.enemySet.frameStartY,
+      this.enemySet.spriteWidth,
+      this.enemySet.spriteHeight,
+      correctX,
+      correctY,
+      this.enemySet.spriteWidth,
+      this.enemySet.spriteHeight
+    );
+
+    this.enemySet.holdCounter++;
+    if (this.enemySet.holdCounter % this.enemySet.hold === 0) {
+      this.currentFrame++;
+    }
+    if (this.currentFrame === this.enemySet.frames - 1) {
+      this.currentFrame = 0;
+    }
   }
   update() {
     this.draw();
