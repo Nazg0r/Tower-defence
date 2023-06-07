80cc2f0ac8f478e998b981e198490b83a7dce3ff
Nazg0r <gorchinshiy83@gmail.com>
2023-06-08 00:31:00 +0300

    added firing mechanics

--- src/core/towers/classTower.js
+++ src/core/towers/classTower.js
@@ -1,15 +1,35 @@
+import Projectile from '../projectiles/classProjectile';
+import { SETTINGS } from '../map/settings';
 export default class Tower {
-  constructor(x, y, ctx, towerSet) {
+  constructor(x, y, ctx, towerSet, target = null) {
     this.x = x;
     this.y = y;
+    this.target = target;
     this.ctx = ctx;
     this.image = new Image();
     this.image.src = towerSet.imageSrc;
+    this.frames = 0;
+    this.weaponPos = {
+      x: this.x + SETTINGS.TAIL_SIZE / 2,
+      y: this.y - 10,
+    };
+    this.projectiles = [];
     this.towerSet = {
+      attackSpeed: towerSet.attackSpeed,
+      range: towerSet.range,
       spriteY: towerSet.spriteY,
       spriteWidth: towerSet.spriteWidth,
       spriteHeight: towerSet.spriteHeight
     };
+  }
+
+  showRange() {
+    this.ctx.beginPath();
+    this.ctx.arc(this.weaponPos.x, this.weaponPos.y, this.towerSet.range, 0, Math.PI * 2);
+    this.ctx.fillStyle = 'rgba(0,255,38,0.05)';
+    this.ctx.fill();
+    this.ctx.fillStyle = 'rgba(94,94,94,0.08)';
+    this.ctx.stroke();
   }
 
   draw() {
@@ -30,5 +50,17 @@ export default class Tower {
 
   update() {
     this.draw();
+    if (this.frames % this.towerSet.attackSpeed === 0 && this.target) {
+      const projectile = new Projectile(
+        this.weaponPos.x,
+        this.weaponPos.y,
+        this.ctx,
+        this.target
+      );
+
+      this.projectiles.push(projectile);
+    }
+
+    this.frames++;
   }
 }
--- src/core/towers/drawTower.js
+++ src/core/towers/drawTower.js
@@ -1,5 +1,6 @@
 import Tower from './classTower';
 import { SETTINGS } from '../map/settings';
+import { enemies } from '../enemys/drawEnemies';
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
@@ -9,13 +10,44 @@ for (let i = 0; i < SETTINGS.ROWS; i++) {
   towers[i] = [];
 }
 export function drawTowers() {
-  console.log(towers);
   for (const row of towers) {
     for (const tower of row) {
       tower.update();
+      tower.target = null;
+
+      const possibleTargets = enemies.filter((enemy) => {
+        const xDifference = enemy.x - tower.weaponPos.x;
+        const yDifference = enemy.y - tower.weaponPos.y;
+        const distance = Math.hypot(xDifference, yDifference);
+
+        return distance < tower.towerSet.range;
+      });
+      tower.target = possibleTargets[0];
+
+
+      const projectileNum = tower.projectiles.length;
+      for (let i = projectileNum - 1; i >= 0; i--) {
+        const projectile = tower.projectiles[i];
+        projectile.update();
+
+        const xDifference = projectile.target.x - projectile.x;
+        const yDifference = projectile.target.x - projectile.x;
+
+        const distance = Math.hypot(xDifference, yDifference);
+        if (distance < projectile.target.enemySet.spriteWidth / 10) {
+          tower.projectiles.pop();
+        }
+      }
     }
   }
+}
 
+export function drawTowersRange() {
+  for (const row of towers) {
+    for (const tower of row) {
+      tower.showRange();
+    }
+  }
 }
 
 export function buildTower(type, towerSets, activeTile) {
@@ -23,7 +55,6 @@ export function buildTower(type, towerSets, activeTile) {
   const towerSet = towerSets[type];
   const tower = new Tower(activeTile.x, activeTile.y, ctx, towerSet);
 
-
   towers[row].push(tower);
-
 }
+
